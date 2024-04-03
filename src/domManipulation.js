5f19dfe240deefa808645e54ca77e30c19676d38
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-04-03 18:57:28 +0200

    Adds the functionality to render the created projects on the Projects tab

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -56,4 +56,55 @@ export default class domManipulation {
         }
     }
 
+    populateProjects(obj) {
+        this.screenUpdate();
+
+        for (let key in obj.projects) {
+            const projectCard = document.createElement("div");
+            const name = document.createElement("p");
+            const description = document.createElement("p");
+            const inspectBtn = document.createElement("button");
+            const deleteBtn = document.createElement("button");
+            const editBtn = document.createElement("button");
+
+            projectCard.classList.add("project-card");
+            name.classList.add("project-name");
+            description.classList.add("project-description");
+            inspectBtn.classList.add("inspectBtn");
+            deleteBtn.classList.add("deleteBtn");
+            editBtn.classList.add("editBtn");
+
+            name.textContent = obj.projects[key].name;
+            description.textContent = obj.projects[key].description;
+            projectCard.id = obj.projects[key].id;
+            projectCard.append(
+                name,
+                description,
+                inspectBtn,
+                editBtn,
+                deleteBtn
+            );
+            this.wrapper.append(projectCard);
+
+            inspectBtn.addEventListener("click", (e) => {
+                this.screenUpdate();
+
+                this.createBtn.classList.add("projTasks");
+                this.createBtn.value = e.target.parentElement.id;
+                this.inspectView(obj);
+            });
+
+            editBtn.addEventListener("click", (e) => {
+                this.createBtn.classList.add("edit");
+                this.createBtn.value = e.target.parentElement.id;
+                this.dialog.showModal();
+            });
+
+            deleteBtn.addEventListener("click", (e) => {
+                this.wrapper.removeChild(e.target.parentElement);
+                delete obj[e.target.parentElement.id];
+            });
+        }
+    }
+
 }
